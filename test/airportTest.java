80f0cd11e53f4f0e8bcc49dda15c0ca1593d2ff6
Cal <cal7907@hotmail.co.uk>
2018-03-16 16:07:50 +0000

    Add test that land returns a message

--- test/airportTest.java
+++ test/airportTest.java
@@ -1,8 +1,19 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.*;
 
 class airportTest {
 
-    
+    public airport tester;
+
+    @BeforeEach
+    public void initialize(){
+        tester = new airport();
+    }
+
+    @Test
+    public void landReturnsAMessage() {
+        assertEquals("Plane landed", tester.land(), "Landing planes should return a message");
+    }
 
 }
